da783da25c451a0cd94779a70e6e1f2ce957f282
Praymore Omeni <omenipraymore18@gmail.com>
2026-03-07 21:07:09 +0000

    Finished exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+        string grade;
+
+        if(gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if(gradePercentage >= 80)
+        {
+            grade = "B";
+
+        }
+        else if(gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if(gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
