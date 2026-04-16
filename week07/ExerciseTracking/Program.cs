2b5a5f21540c44ba9d2ccbf5d643925440e705d2
Praymore Omeni <omenipraymore18@gmail.com>
2026-04-16 20:24:59 +0000

    Complete exercise tracking program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("13 Apr 2026", 50, 2.0));
+        activities.Add(new Cycling("21 Apr 2026", 10, 7.0));
+        activities.Add(new Swimming("01 Apr 2026", 30, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
